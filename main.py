3e26d6bc3d49144c4ffdf2cc4c015bee05a2265e
SreeshnaC <sreeshnacholayil07@gmail.com>
2024-12-12 14:59:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# from graphics.rectangle import *
+# area=area(5,6)
+# print(area)
+# perimeter=perimeter(5,6)
+# print(perimeter)
+
+import graphics.rectangle as re
+import graphics.circle as c
+print("rectangle")
+print("area:",re.area(5,6))
+print("perimeter:",re.perimeter(5,6))
+print("circle")
+print("area:",c.area(4))
+print("perimeter:",c.perimeter(4))
+
